86223c291a0d10865cd11fd98dea2274bc928707
Benny <86641253+thejourneyville@users.noreply.github.com>
2021-09-24 14:49:27 +0900

    Update two_joggers.py
    passed

--- 6kyu/two_joggers.py
+++ 6kyu/two_joggers.py
@@ -1,47 +1,18 @@
 def nbr_of_laps(x, y):
-    prod = x * y
-
-    x_lap = prod // x
-    y_lap = prod // y
-
-    return x_lap, y_lap
-
-
-"""
-#include <stdio.h>
-
-typedef struct Laps_Pairing {
-    int bob;
-    int charles;
-} laps;
-
-laps nbr_of_laps(int x, int y) {
-  int product = x * y;
-
-  int count = 1;
-  int x_lap = 0;
-  int y_lap = 0;
-  while (count <= product) {
-    if (count % x == 0) {
-      x_lap = count;
-      break;
-      }
-    count++;
-  }
-  count = 1;
-  while (count <= product) {
-    if (count % y == 0) {
-      y_lap = count;
-      break;
-    }
-    count++;
-  }
-
-  struct Laps_Pairing results;
-
-  results.bob = x_lap;
-  results.charles = y_lap;
-  return results;
-
-}
-"""
+    
+    if x == y:
+        return 1, 1
+    
+    else:
+        
+        product = x * y
+        max_x, max_y = product / x, product / y
+        divisor = max([max_x, max_y]) - 1
+        
+        while divisor > 1:
+            if not max_x % divisor and not max_y % divisor:
+                return max_x / divisor, max_y / divisor
+            else:
+                divisor -= 1
+        
+        return max_x, max_y
